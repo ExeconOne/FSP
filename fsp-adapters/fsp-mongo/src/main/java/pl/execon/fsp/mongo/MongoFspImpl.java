30d998a73567074b55d9da9c6d044febaa899442
Łukasz Pietrzykowski <lukasz.pietrzykowski@execon.pl>
2022-05-27 12:27:12 +0200

    EXEOCOMMONS-3 wersja 1.0

--- fsp-adapters/fsp-mongo/src/main/java/pl/execon/fsp/mongo/MongoFspImpl.java
+++ fsp-adapters/fsp-mongo/src/main/java/pl/execon/fsp/mongo/MongoFspImpl.java
@@ -4,34 +4,27 @@ import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.stereotype.Component;
 import pl.execon.fsp.core.FspRequest;
 import pl.execon.fsp.core.FspResponse;
 
 import java.util.List;
 
-@Component
 @RequiredArgsConstructor
-public class MongoFspImpl<T> implements MongoFspRepository<T> {
+public class MongoFspImpl<T> implements MongoFsp<T> {
 
     @NonNull
     private MongoTemplate mongoTemplate;
 
     @Override
-    public FspResponse<T> findFsp(FspRequest request) {
+    public FspResponse<T> findFsp(FspRequest request, Class<T> documentClass) {
         Query query = new MongoFspRequestResolver(request).asQuery();
-        List<T> list = mongoTemplate.find(query, getClazzType());
+        List<T> list = mongoTemplate.find(query, documentClass);
 
         if (!request.hasPageInfo()) {
             return new FspResponse<>(list);
         }
 
-        long count = mongoTemplate.count(Query.of(query).limit(-1).skip(-1), this.getClass().getGenericSuperclass().getClass());
+        long count = mongoTemplate.count(Query.of(query).limit(-1).skip(-1), documentClass);
         return new FspResponse<>(request, list, count);
-    }
-
-    @Override
-    public Class<T> getClazzType() {
-        return null;
     }
 }
--- fsp-core/src/main/java/pl/execon/fsp/core/FspRepository.java
+++
@@ -1,6 +0,0 @@
-package pl.execon.fsp.core;
-
-public interface FspRepository<T> {
-
-    FspResponse<T> findFsp(FspRequest request);
-}
